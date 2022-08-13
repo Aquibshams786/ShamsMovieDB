60ca9fe931fd1f3efd828254d3ba3a8d4c67bb5b
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-08-13 21:40:40 +0530

    my first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+import Header from './components/Header';
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <GlobalStyle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import RMDBLogo from '../../images/shams1.png';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+import { Wrapper } from "./Header.styles";
+import { LogoImg } from "./Header.styles";
+import { TMDBLogoImg } from "./Header.styles";
+import { Content } from "./Header.styles";
+
+const Header=()=>(
+    <Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo}alt=""/>
+            <TMDBLogoImg src={TMDBLogo}/>
+        </Content>
+    </Wrapper>
+);
+
+export default Header;
+
+
