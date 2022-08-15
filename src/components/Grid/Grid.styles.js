c9f7abe8090f648bb83ea2980d0eded0d81e5a6b
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-08-15 07:29:55 +0530

    udapted grid style

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -20,4 +20,7 @@ export const Content = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-gap: 2rem;
+  @media screen and (max-width: 768px) {
+      grid-template-columns: auto auto;
+    }
 `;
